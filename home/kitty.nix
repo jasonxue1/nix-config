c474c78b7c7cdece9f1589f764f402576c39a7e1
jasonxue <jason@xuechunxi.com>
2025-08-23 23:03:59 +0800

    chore: make kitty useful

--- home/kitty.nix
+++ home/kitty.nix
@@ -8,25 +8,43 @@
       italic_font = "auto";
       font_size = "15.0";
       force_ltr = "yes";
+      disable_ligatures = "always";
 
       cursor_trail = 4;
       mouse_hide_wait = 2.0;
 
-      tab_bar_edge = "top";
-      tab_bar_style = "powerline";
-
       hide_window_decorations = "titlebar-only";
-      window_padding_width = 15;
-      remember_window_size = "yes";
+      window_padding_width = 5;
+      enable_audio_bell = "no";
 
       macos_option_as_alt = "yes";
       macos_quit_when_last_window_closed = "yes";
+      macos_window_resizable = "no";
+      remember_window_size = "yes";
 
       shell = "${pkgs.nushell}/bin/nu";
 
       background_opacity = "1.0";
       allow_remote_control = "yes";
-      disable_ligatures = "always";
+
+      tab_bar_edge = "top";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
+      tab_title_template = "'({index}) {title}'";
+
+      kitty_mod = "ctrl+shift";
+    };
+    keybindings = {
+      "option+c" = "close_window";
+      "option+n" = "next_window";
+      "option+p" = "previous_window";
+      "option+l" = "next_tab";
+      "option+h" = "previous_tab";
+      "option+w" = "new_tab";
+      "cmd+l" = "next_layout";
+      "cmd+h" = "last_used_layout";
+      "cmd+o" = "toggle_layout stack";
+      "option+u" = "open_url_with_hints";
     };
   };
 }
