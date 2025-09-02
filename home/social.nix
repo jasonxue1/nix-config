b82c7fba6e61f1dd935377d8a5f059519c648a7e
jasonxue <jason@xuechunxi.com>
2025-09-02 23:28:02 +0800

    feat: add iamb for matrix

--- home/social.nix
+++ home/social.nix
@@ -2,4 +2,19 @@
   home.packages = with pkgs; [
     qq
   ];
+
+  programs.iamb = {
+    enable = true;
+    settings = {
+      default_profile = "jason";
+      profiles = {
+        jason.user_id = "@jasonxue_1:matrix.org";
+      };
+      settings = {
+        image_preview = {
+          protocol.type = "kitty";
+        };
+      };
+    };
+  };
 }
