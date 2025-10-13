d9f4e1a3b277fb69a5b2645fc7fb9bc70f3c2c9a
jasonxue <jason@xuechunxi.com>
2025-10-14 01:30:13 +0800

    fix: update nvim config

--- base/dev/neovim/nvim/lua/lang/lua.lua
+++ base/dev/neovim/nvim/lua/lang/lua.lua
@@ -20,7 +20,6 @@ return {
       local null_ls = require "null-ls"
       opts.sources = require("astrocore").list_insert_unique(opts.sources, {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.selene,
       })
     end,
   },
