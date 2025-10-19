dcf237c1558ae020eeb1ea4a6ae63eb6268c2ebe
jasonxue <jason@xuechunxi.com>
2025-10-19 16:46:14 +0800

    feat: add yamllint

--- base/dev/lang/yaml.nix
+++ base/dev/lang/yaml.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     yaml-language-server
+    yamllint
   ];
 }
--- base/dev/neovim/nvim/lua/lang/yaml.lua
+++ base/dev/neovim/nvim/lua/lang/yaml.lua
@@ -1,16 +1,29 @@
 ---@type LazySpec
 return {
   {
-    "Astronvim/astrolsp",
+    {
+      "Astronvim/astrolsp",
+      opts = function(_, opts)
+        opts.servers =
+          require("astrocore").list_insert_unique(opts.servers, { "yamlls" })
+        opts.formatting = {
+          disabled = require("astrocore").list_insert_unique(
+            opts.formatting.disabled,
+            { "yamlls" }
+          ),
+        }
+      end,
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
     opts = function(_, opts)
-      opts.servers =
-        require("astrocore").list_insert_unique(opts.servers, { "yamlls" })
-      opts.formatting = {
-        disabled = require("astrocore").list_insert_unique(
-          opts.formatting.disabled,
-          { "yamlls" }
-        ),
-      }
+      local null_ls = require "null-ls"
+
+      opts.sources = require("astrocore").list_insert_unique(opts.sources, {
+        null_ls.builtins.diagnostics.yamllint,
+      })
     end,
   },
 }
